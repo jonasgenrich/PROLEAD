5f456bdf61836ecf29339507104e7a5ac0c6178b
jonas <jonas.genrich@ruhr-uni-bochum.de>
2024-02-07 08:26:18 +0100

    more example code added to SW_test

--- examples/Software/SW_test/source/test.s
+++ examples/Software/SW_test/source/test.s
@@ -6,7 +6,11 @@
 .align 4
 .global   test
 .type   test, %function;
-test: // test(input_s0_local, input_s1_local)
+
+test:
+    b testD
+
+testA: // test(input_s0_local, input_s1_local)
     push {r4-r11,lr}
     mov r2, #0
     mov r3, #0
@@ -14,9 +18,66 @@ test: // test(input_s0_local, input_s1_local)
     ldr r2, [r0]
     mov r3, #1
     cmp r3, #1
-    beq skip
+    beq skipA
     ldr r2, [r1]
-    skip:
+    skipA:
+    mov r3, #123
+    eor r2, r3
+    mov r0, r3
+    
+    pop {r4-r11,lr}
+    bx lr
+
+testB: // test(input_s0_local, input_s1_local)
+    push {r4-r11,lr}
+    mov r2, #0
+    mov r3, #0
+
+    mov r3, #1
+    cmp r3, #1
+    beq skipB
+    ldr r2, [r1]
+    skipB:
+    ldr r2, [r0]
+    mov r3, #123
+    eor r2, r3
+    mov r0, r3
+    
+    pop {r4-r11,lr}
+    bx lr
+
+testC: // test(input_s0_local, input_s1_local)
+    push {r4-r11,lr}
+    mov r2, #0
+    mov r3, #0
+
+    mov r3, #1
+    cmp r3, #1
+    beq skipC
+    ldr r2, [r1]
+    mov r2, #0
+    skipC:
+    ldr r2, [r0]
+    mov r3, #123
+    eor r2, r3
+    mov r0, r3
+    
+    pop {r4-r11,lr}
+    bx lr
+
+testD: // test(input_s0_local, input_s1_local)
+    push {r4-r11,lr}
+    mov r2, #0
+    mov r3, #0
+
+    mov r3, #1
+    cmp r3, #1
+    beq skipD
+    ldr r2, [r1, #4]
+    ldr r2, [r1]
+    mov r2, #0
+    skipD:
+    ldr r2, [r0]
     mov r3, #123
     eor r2, r3
     mov r0, r3
